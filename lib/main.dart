9dbf4840e62db59f15ade814f69504745c85645f
Yair Rfz <yairrdz11@gmail.com>
2023-06-13 22:23:26 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:components/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Components App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Components'),
+        ),
+        body: HomePageTemp()
+      ),
+    );
+  }
+}
