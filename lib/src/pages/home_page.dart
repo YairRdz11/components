27644697f2b5ad8fee4721ab389800da636f2a46
Yair Rfz <yairrdz11@gmail.com>
2023-06-22 22:03:24 -0600

    adding data

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:components/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,6 +15,7 @@ class HomePage extends StatelessWidget {
   }
   
   Widget _list() {
+    print(menuProvider.options);
     return ListView(
       children: _buildListItems(),
     );
