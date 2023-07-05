ede2bfbfc5228562ceb5f58c5fa2594a6951cd83
Yair Rfz <yairrdz11@gmail.com>
2023-07-04 22:50:45 -0600

    Adding alert dialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,12 +8,50 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert page')
       ),
+      body: Center(
+        child: FilledButton(
+          onPressed: () => _showAlert(context), 
+          child: const Text("Mostrar alerta"),
+        ) 
+      ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_location),
+        child: const Icon(Icons.add_location),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: const Text('Titulo'),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('This is a box alert content'),
+              FlutterLogo(size: 100.0)
+            ],
+          ),
+          actions: <Widget>[
+            FilledButton(
+              child: const Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FilledButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      } 
     );
   }
 }
