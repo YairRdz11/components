34836a6c027879c801000e84d393a9ea87423611
Yair Rfz <yairrdz11@gmail.com>
2023-07-04 22:09:33 -0600

    Adding image cards

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -11,7 +11,11 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_cardType1()],
+        children: <Widget>[
+          _cardType1(),
+          SizedBox(height: 30.0),
+          _cardType2()
+          ],
       ),
     );
   }
@@ -39,6 +43,26 @@ class CardPage extends StatelessWidget {
             ],
           )
         ]
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'), 
+            image: NetworkImage('https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Esto es una card'),
+            )
+        ],
       ),
     );
   }
